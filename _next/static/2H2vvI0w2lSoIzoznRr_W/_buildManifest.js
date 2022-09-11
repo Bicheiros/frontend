e2494986806cfac8173aead79705a1efa8a5f157
TulioSantos <tulio.santos1@nutrien.com>
2022-09-10 22:12:40 -0300

    v2

---
+++ _next/static/2H2vvI0w2lSoIzoznRr_W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b233e27782e104ad.css","static/chunks/pages/index-0c3bd8624b936b83.js"],"/_error":["static/chunks/pages/_error-25bbc618360854a5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
