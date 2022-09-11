548d6c4b1ea0c19502922382a19373c6f510f6b3
TulioSantos <tulio.santos1@nutrien.com>
2022-09-11 08:47:09 -0300

    v1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import Footer from '../components/Footer';
 import Header from '../components/Header';
 import App from '../components/App';
 
@@ -16,8 +15,6 @@ export default function Home() {
       <Header />
 
       <App />
-
-      <Footer />
     </div>
   );
 }
